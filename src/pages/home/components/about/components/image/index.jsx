6d294e52d0297f53210556dd3a7270419976676e
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-06 02:59:42 +0300

    dokuz

--- src/pages/home/components/about/components/image/index.jsx
+++ src/pages/home/components/about/components/image/index.jsx
@@ -2,7 +2,7 @@ const Image = ({ data }) => {
     return (
         <div className="flex justify-center w-full">
             <img
-                src={data.about[0].image}
+                src={`https://psi-back.onrender.com${data.about[0].image}`}
                 alt="About Us"
                 className="object-cover w-full h-72 sm:h-96 md:h-full lg:max-h-[400px] rounded-lg shadow-lg"
             />
@@ -11,3 +11,4 @@ const Image = ({ data }) => {
 };
 
 export default Image;
+
