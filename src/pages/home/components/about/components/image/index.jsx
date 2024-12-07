73ec986fe00eb6fc68d03005d2ee4c87d1b4df95
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-07 14:06:30 +0300

    yirmüçi

--- src/pages/home/components/about/components/image/index.jsx
+++ src/pages/home/components/about/components/image/index.jsx
@@ -1,14 +1,21 @@
 const Image = ({ data }) => {
     return (
-        <div className="flex justify-center w-full">
-            <img
-                src={`https://psi-back.onrender.com${data.about[0].image}`}
-                alt="About Us"
-                className="object-cover w-full h-72 sm:h-96 md:h-full lg:max-h-[400px] rounded-lg shadow-lg"
-            />
-        </div>
+      <div className="flex justify-center w-full">
+        {/* Image for large screens and larger */}
+        <img
+          src={`https://psi-back.onrender.com${data.about[0].image}`}
+          alt="About Us"
+          className="hidden lg:block object-cover w-full h-72 sm:h-96 md:h-full lg:max-h-[400px] rounded-lg shadow-lg"
+        />
+        {/* Phone image for smaller screens */}
+        <img
+          src={`https://psi-back.onrender.com${data.about[0].phone_image}`}
+          alt="About Us (Phone)"
+          className="block lg:hidden object-cover w-full h-72 sm:h-96 md:h-full lg:max-h-[400px] rounded-lg shadow-lg"
+        />
+      </div>
     );
-};
-
-export default Image;
-
+  };
+  
+  export default Image;
+  
