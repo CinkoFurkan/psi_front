10649aa09daf00aae8c968731915fa93e463bfe7
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-29 22:38:44 +0300

    color

--- src/components/spinner/index.jsx
+++ src/components/spinner/index.jsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-white  z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-[#f4efe6]  z-50">
             <div className="w-16 h-16 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-red-500">
             </div>
         </div>
