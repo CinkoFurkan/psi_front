05a809bb856aa6e220f46cd687a1804dfe177f21
liderbektas <132711250+liderbektas@users.noreply.github.com>
2024-12-29 19:07:12 +0300

    Fixed footer

--- src/layout/web/index.jsx
+++ src/layout/web/index.jsx
@@ -1,38 +1,16 @@
 import Header from "./components/header";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Footer from "./components/footer";
-import { Toaster } from "react-hot-toast";
-import { useEffect, useState } from "react";
+import {Toaster} from "react-hot-toast";
 
 export default function WebLayout() {
-    const [showSubscription, setShowSubscription] = useState(false);
-
-    const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const documentHeight = document.documentElement.scrollHeight;
-        const windowHeight = window.innerHeight;
-
-        if (scrollPosition + windowHeight >= documentHeight - 50) {
-            setShowSubscription(true);
-        } else {
-            setShowSubscription(false);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
 
     return (
         <div>
-            <Toaster />
-            <Header />
-            <Outlet />
-            {showSubscription && <Footer />}
+            <Toaster/>
+            <Header/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 }
